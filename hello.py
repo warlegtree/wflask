439e4f30e8a99f8c0cad2e681b7e73847c7072e2
ezngzen <28847179@qq.com>
2016-01-19 15:04:07 +0800

    add request  3a

--- hello.py
+++ hello.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python
 
 from flask import Flask
+# import request
+from flask import request
 
 app = Flask(__name__)
 
-
+#index
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
+
+#request and response
+@app.route('/request')
+def req():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is %s</p>' % user_agent
+
+#User, dynamic
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
 
 
 if __name__=='__main__':
